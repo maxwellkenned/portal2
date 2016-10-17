51d73ce6dcde5bac7b4134bd0684e212ef45645a
Maxwell Kenned <kenned123@gmail.com>
2016-10-17 01:06:34 -0200

    Alterado o Banco local

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 module.exports = function(uri) {
 //conexão com o mongodb
-//mongoose.connect('mongodb://admin:82546459@ds019472.mlab.com:19472/portal', function (err) {
-mongoose.connect(uri, function (err) {
+mongoose.connect('mongodb://admin:82546459@ds019472.mlab.com:19472/portal', function (err) {
+//mongoose.connect(uri, function (err) {
     'use strict';
     if (err) {
         console.log("Erro ao conectar mongodb: " + err);
