42b0e4414c171e729484b48b5f5ae9a443cff8c7
Maxwell Kenned <kenned123@gmail.com>
2016-11-14 02:10:31 -0200

    Atualizado modo de exibição da notificação de registro e login; Adicionado validação no lado do cliente antes de fazer o registro

--- public/js/login/validar.js
+++ public/js/login/validar.js
@@ -1,17 +1,46 @@
 $('#loginform').submit(function () {
-      var senha  = $('#login-password').val().length;
-      var email = $('#login-username').val().length;
-      if(email < 1 && senha < 1){
-        $.notify('E-mail e senha não informados', 'error');
-        $('#login-username').focus();
-        return false;
-      } else if(email < 1){
-        $.notify('E-mail não informada', 'error');
-        $('#login-username').focus();
-        return false;
-      } else if(senha <1){
-        $('#login-password').notify('Senha não informada','error', { position:"bottom"});
-        $('#login-password').focus();
-        return false;
-      }
-    });
+  let senha  = $('#login-password').val().length;
+  let email = $('#login-username').val().length;
+  if(email < 1 && senha < 1){
+    $('#login-username').notify('E-mail e senha não informados', 'error');
+    $('#login-username').focus();
+    return false;
+  } else if(email < 1){
+    $('#login-username').notify('E-mail não informada', 'error');
+    $('#login-username').focus();
+    return false;
+  } else if(senha <1){
+    $('#login-password').notify('Senha não informada','error', { position:"bottom"});
+    $('#login-password').focus();
+    return false;
+  }
+});
+$('#register-form').submit(function(){
+  let nome =  $('#nome').val();
+  let sobrenome =  $('#sobrenome').val();
+  let email =  $('#email').val();
+  let senha =  $('#senha').val();
+  let confirSenha = $('#confirma_senha').val();
+  
+  if(nome.length < 1){
+    $('#nome').notify('Infome o nome', 'error');
+    $('#nome').focus();
+    return false;
+  } else if(sobrenome.length < 1){
+    $('#sobrenome').notify('Infome o sobrenome', 'error');
+    $('#sobrenome').focus();
+    return false;
+  }else if(senha.length < 1 ){
+    $('#senha').notify('Infome a senha', 'error');
+    $('#senha').focus();
+    return false;
+  }else if(confirSenha.length < 1){
+    $('#confirma_senha').notify('Confirme a senha', 'error');
+    $('#confirma_senha').focus();
+    return false;
+  }else if (senha !== confirSenha){
+    $('#confirma_senha').notify('Senha de confirmação não confere', 'error');
+    $('#confirma_senha').focus();
+    return false;
+  }
+});
