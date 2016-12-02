95cbdf1a72f6ac57ab21534bfa2d692e464cb536
Maxwell Kenned <kenned123@gmail.com>
2016-12-02 02:09:29 -0300

    Corrigido função de criar Pasta; Corrigido função de acessar pasta; Corrigido função de upload de arquivo por pasta; Adicionado contatos online ao chat;

--- multiroom_chat/app/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.home = function(application, req, res){
-
-	res.render("index", {validacao: {}});
-}
--- multiroom_chat/app/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(application){
-	application.get('/', function(req, res){
-		application.app.controllers.index.home(application, req, res);
-	});
-}
